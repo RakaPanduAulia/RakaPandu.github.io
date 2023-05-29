6ef372cbfba51076469915f8621a638a1abdf907
RakaPanduAulia <107733755+RakaPanduAulia@users.noreply.github.com>
2023-05-29 12:36:50 +0700

    Update conf.php

--- services/conf.php
+++ services/conf.php
@@ -7,9 +7,9 @@ class CONF {
 
     /* Data configuration for database */
     public $DB_SERVER   = "localhost";
-    public $DB_USER     = "appsians_amin";
-    public $DB_PASSWORD = "appsians_amin";
-    public $DB_NAME     = "appsians_testapi";
+    public $DB_USER     = "";
+    public $DB_PASSWORD = "";
+    public $DB_NAME     = "pt02";
 
     /* [ IMPORTANT ] be careful when edit this security code, use AlphaNumeric only*/
     /* This string must be same with security code at Android, if its different android unable to submit order */
